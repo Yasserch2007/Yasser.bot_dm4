7873c234887df87b21d3d23b09f089a344ff770b
Yasserch2007 <108551737+Yasserch2007@users.noreply.github.com>
2022-07-03 14:44:22 +0100

    Update config.js

--- config.js
+++ config.js
@@ -42,22 +42,22 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
 global.owner = ['212675012435'] //ur owner number
-global.ownername = "ArullOfc"
+global.ownername = "Yasserbot"
 global.ytname = "YT:ARULL OFC " //ur yt chanel name
 global.socialm = "GitHub: Warikrr" //ur github or insta name
 global.location = "Indonesia, Kalimantan Selatan, Kotabaru" //ur location
 
 //bot bombdy
-global.botname = "ArullBotz" //ur bot name
+global.botname = "YasserBotz" //ur bot name
 global.websitex = "https://youtube.com/channel/UC1ikkqsPvtWuE_QonsET1yg" //ur website
 global.vidmenu = { url: 'https://a.uguu.se/eFNTvNt.mp4' } //gif and video menu
 global.packname = "Sticker By"
-global.author = "ArullBotz"
+global.author = "YasserBotz"
 global.themeemoji = "🐶"
 global.reactmoji = "🗿"
 global.ownertag = ['ArullOfc'] //ur owner tag
 global.ownernummenu = ['212675012435'] //ur owner number in menu and all
-global.watermark = "ArullBotz" //ur watermark
+global.watermark = "YasserBotz" //ur watermark
 global.botscript = "https://youtu.be/Gr6yuCbSGTI"
 global.linkz1 = "https://chat.whatsapp.com/F6CAAOhrORm1sPdq6cRKAS"
 global.linkz2 = "https://youtube.com/channel/UC1ikkqsPvtWuE_QonsET1yg"
